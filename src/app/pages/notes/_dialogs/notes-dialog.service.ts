c34f1c5009715a55118d8d98ac397c4716890ba0
Humberd <humberd2@gmail.com>
2019-08-03 10:35:04 +0200

    Changed to conent as normal textarea

--- src/app/pages/notes/_dialogs/notes-dialog.service.ts
+++ src/app/pages/notes/_dialogs/notes-dialog.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 import { CreateNoteComponent } from './create-note/create-note.component';
 
 @Injectable()
 export class NotesDialogService {
+  defaultConfig: MatDialogConfig<any> = {
+    width: '1000px'
+  };
 
   constructor(private dialogService: MatDialog) {
 
   }
 
   openCreateDialog() {
-    return this.dialogService.open(CreateNoteComponent);
+    return this.dialogService.open(CreateNoteComponent, {...this.defaultConfig});
   }
 }
