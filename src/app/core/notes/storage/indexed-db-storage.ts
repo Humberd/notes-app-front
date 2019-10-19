1ed8e7052c7bbe01f8ecc9846be3524aec2433e4
Humberd <humberd2@gmail.com>
2019-10-19 14:30:25 +0200

    Save commit

---
+++ src/app/core/notes/storage/indexed-db-storage.ts
@@ -0,0 +1,114 @@
+import { Note, NoteCreate, NoteUpdate } from '../../../models/note';
+import { Observable } from 'rxjs';
+import { NotesStorage } from '../notes-storage';
+
+export class IndexedDbStorage implements NotesStorage {
+  private db: IDBDatabase;
+
+  constructor() {
+    const dbRequest = window.indexedDB.open('NotesAppDatabase', 1);
+    dbRequest.onsuccess = ev => {
+      this.db = dbRequest.result;
+    };
+
+    dbRequest.onerror = ev => {
+      throw new Error('IndexedDb Storage cannot be opened');
+    };
+
+    dbRequest.onupgradeneeded = ev => {
+      this.db = dbRequest.result;
+      this.db.createObjectStore('notes', {keyPath: 'id', autoIncrement: true});
+    };
+  }
+
+  delete(noteId: string): Observable<void> {
+    return new Observable<void>(subscriber => {
+      const tx = this.db.transaction('notes', 'readwrite');
+      const store = tx.objectStore('notes');
+
+      const request = store.delete(noteId);
+
+      tx.onerror = ev => {
+        subscriber.error(ev);
+      };
+
+      tx.oncomplete = ev => {
+        subscriber.next();
+        subscriber.complete();
+      };
+    });
+  }
+
+  read(noteId: string): Observable<Note> {
+    return new Observable<Note>(subscriber => {
+      const tx = this.db.transaction('notes', 'readonly');
+      const store = tx.objectStore('notes');
+
+      const request = store.get(noteId);
+
+      tx.onerror = ev => {
+        subscriber.error(ev);
+      };
+
+      tx.oncomplete = ev => {
+        subscriber.next(request.result);
+        subscriber.complete();
+      };
+    });
+  }
+
+  readAll(): Observable<Note[]> {
+    return new Observable<Note[]>(subscriber => {
+      const tx = this.db.transaction('notes', 'readonly');
+      const store = tx.objectStore('notes');
+
+      const request = store.getAll();
+
+      tx.onerror = ev => {
+        subscriber.error(ev);
+      };
+
+      tx.oncomplete = ev => {
+        subscriber.next(request.result);
+        subscriber.complete();
+      };
+    });
+  }
+
+  add(note: NoteCreate): Observable<Note> {
+    return new Observable<Note>(subscriber => {
+      const tx = this.db.transaction('notes', 'readwrite');
+      const store = tx.objectStore('notes');
+
+      const request = store.add(note);
+
+      tx.onerror = ev => {
+        subscriber.error(ev);
+      };
+
+      tx.oncomplete = ev => {
+        subscriber.next({...note, id: String(request.result)});
+        subscriber.complete();
+      };
+    });
+  }
+
+  update(note: NoteUpdate): Observable<Note> {
+    return new Observable<Note>(subscriber => {
+      const tx = this.db.transaction('notes', 'readwrite');
+      const store = tx.objectStore('notes');
+
+      const request = store.put(note);
+
+      tx.onerror = ev => {
+        subscriber.error(ev);
+      };
+
+      tx.oncomplete = ev => {
+        subscriber.next({...note, id: String(request.result)});
+        subscriber.complete();
+      };
+    });
+  }
+
+}
