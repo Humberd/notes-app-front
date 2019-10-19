e89113d3c9ab66551fac3533c91cc3f3ecd9e50d
Humberd <humberd2@gmail.com>
2019-10-19 21:52:47 +0200

    Note type is now correctly replaces while preserving noteId

--- src/app/views/home/_services/note-id-route-param.ts
+++ src/app/views/home/_services/note-id-route-param.ts
@@ -1,9 +1,11 @@
 import { RouteParam } from '@ng-boost/core';
 import { Injectable } from '@angular/core';
 
+export const noteIdParamName = 'noteId';
+
 @Injectable()
 export class NoteIdRouteParam extends RouteParam {
   protected paramName(): string {
-    return 'noteId';
+    return noteIdParamName;
   }
 }
--- src/app/views/home/general-list/general-list.component.ts
+++ src/app/views/home/general-list/general-list.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { NoteTypeRouteParam } from '../_services/note-type-route-param';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Destroy$ } from '@ng-boost/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-general-list',
@@ -7,11 +12,34 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GeneralListComponent implements OnInit {
+  @Destroy$() private readonly destroy$ = new Subject();
 
-  constructor() {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private noteTypeRouteParam: NoteTypeRouteParam,
+    private cdr: ChangeDetectorRef,
+  ) {
   }
 
   ngOnInit() {
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(it => it instanceof NavigationEnd),
+      )
+      .subscribe(() => this.cdr.markForCheck());
+  }
+
+  replaceNoteTypeInPathFor(path: string): string {
+    const urlSegments = this.router.url.split('/');
+    const noteTypeIndex = urlSegments.findIndex(value => value === this.noteTypeRouteParam.value);
+    if (noteTypeIndex < 0) {
+      return `../${path}`;
+    }
+
+    urlSegments[noteTypeIndex] = path;
+    return urlSegments.join('/');
   }
 
 }
