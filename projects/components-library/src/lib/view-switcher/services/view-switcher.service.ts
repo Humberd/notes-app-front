3ee3f72f3ebd3a3462f49ee7d38c84d690195261
Humberd <humberd2@gmail.com>
2020-01-24 20:23:39 +0100

    View switcher is not being persisted across saves

--- projects/components-library/src/lib/view-switcher/services/view-switcher.service.ts
+++ projects/components-library/src/lib/view-switcher/services/view-switcher.service.ts
@@ -6,15 +6,22 @@ import { ViewSwitcherConfig } from 'components-library/lib/view-switcher/models/
 export class ViewSwitcherService {
   availableViews: ViewSwitcherConfig[];
 
-  private readonly _selectedView$ = new BehaviorSubject<string>('code');
+  private readonly _selectedView$ = new BehaviorSubject<string>(localStorage.getItem('notes-app-view-switcher'));
   readonly selectedView$ = this._selectedView$.asObservable();
 
   get selectedView(): string {
     return this._selectedView$.value;
   }
 
+  constructor() {
+    if (!this.selectedView) {
+      this.select('code');
+    }
+  }
+
   select(id: string) {
     this._selectedView$.next(id);
+    localStorage.setItem('notes-app-view-switcher', id);
   }
 
   setAvailableViews(config: ViewSwitcherConfig[]) {
