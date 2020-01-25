e9b3a27faeb812c671a5283eaff8ab032df95bf7
Humberd <humberd2@gmail.com>
2020-01-25 22:31:57 +0100

    Added delete icon

--- src/app/shared/common/tag-options/tag-options.ts
+++ src/app/shared/common/tag-options/tag-options.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { NoteTag } from '../../../domains/note/models/note-tag';
 import { Tag } from '../../../domains/tag/models/tag.model';
 import { TagDialogsService } from '../../../dialogs/tag-dialogs/services/tag-dialogs.service';
+import { filter } from 'rxjs/operators';
+import { ConfirmationDialogResult } from '../../../dialogs/common-dialogs/components/confirmation-dialog/models/confirmation-dialog-result';
 
 @Injectable()
 export class TagOptionsController {
@@ -19,6 +21,19 @@ export class TagOptionsController {
         action: tag => this.tagDialogsService.openEditTagDialog({
           ...tag,
         }),
+      },
+      {
+        icon: 'delete_forever',
+        iconColor: 'warn',
+        labelTK: 'common.delete_permanently',
+        dividerAbove: true,
+        showWhen: tag => true,
+        action: tag => this.tagDialogsService.openDeleteTagPermanentlyDialog(tag)
+          .afterClosed()
+          .pipe(
+            filter(result => result === ConfirmationDialogResult.CONFIRMED),
+          )
+          .subscribe(() => console.log('CONFIRMED')),
       },
     ];
   }
