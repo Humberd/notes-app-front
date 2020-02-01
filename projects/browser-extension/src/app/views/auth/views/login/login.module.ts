d10ca3620d4f956ea668370db1910e5acbd11717
Humberd <humberd2@gmail.com>
2020-02-01 20:09:09 +0100

    Save commit

--- projects/browser-extension/src/app/views/auth/views/login/login.module.ts
+++ projects/browser-extension/src/app/views/auth/views/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { LoginComponent } from './login.component';
   imports: [
     CommonModule,
     LoginRoutingModule,
+    MatButtonModule,
   ],
 })
 export class LoginModule {
