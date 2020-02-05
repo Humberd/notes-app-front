d54f5f0918cdb41604dfe9132aeb0dab332b6d42
Humberd <humberd2@gmail.com>
2020-02-05 22:07:07 +0100

    Added passing selection to the extensions

--- projects/browser-extension/src/assets/content-selection.js
+++ projects/browser-extension/src/assets/content-selection.js
@@ -3,26 +3,49 @@ let hasPanelElem = false;
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
   if (hasPanelElem) {
-    sendResponse('ALREADY_EXISTS');
+    sendResponse({
+      type: 'ALREADY_EXISTS',
+    });
     return;
   }
 
   hasPanelElem = true;
+  let selection;
 
   const {saveButton, cancelButton, panelElem} = createFixedTopPanel();
 
   cancelButton.onclick = () => {
-    sendResponse('CANCEL');
-    panelElem.remove();
+    endSelectionMode({
+      type: 'CANCEL',
+    });
   };
 
   saveButton.onclick = () => {
-    sendResponse('SAVE');
-    panelElem.remove();
+    endSelectionMode({
+      type: 'SAVE',
+      content: selection.toString(),
+    });
   };
 
   document.body.appendChild(panelElem);
 
+  const onSelectionChangeListener = ev => {
+    selection = document.getSelection();
+    if (selection.type === 'Range') {
+      saveButton.style.display = 'inline-block';
+    } else {
+      saveButton.style.display = 'none';
+    }
+  };
+
+  document.addEventListener('selectionchange', onSelectionChangeListener);
+
+  function endSelectionMode(response) {
+    sendResponse(response);
+    panelElem.remove();
+    hasPanelElem = false;
+    document.removeEventListener('selectionchange', onSelectionChangeListener);
+  }
 });
 
 function createFixedTopPanel() {
@@ -36,6 +59,7 @@ function createFixedTopPanel() {
   const saveButton = document.createElement('button');
   saveButton.classList.add('brx-button');
   saveButton.textContent = 'SAVE';
+  saveButton.style.display = 'none';
 
   panelElem.append(cancelButton, saveButton);
 
