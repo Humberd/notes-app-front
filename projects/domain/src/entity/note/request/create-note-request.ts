6f764d2376c474a49d360df37ea648c38a5bb264
Humberd <humberd2@gmail.com>
2020-04-10 15:15:50 +0200

    Added tags auutocomplete

--- projects/domain/src/entity/note/request/create-note-request.ts
+++ projects/domain/src/entity/note/request/create-note-request.ts
@@ -1,5 +1,8 @@
+import { NameModel } from '@domain/common/name-model';
+
 export interface CreateNoteRequest {
   title: string;
-  url: string;
-  content: string;
+  url?: string;
+  content?: string;
+  tags?: NameModel[];
 }
--- projects/web-app/src/app/dialogs/modules/note-modification-dialog/note-modification-dialog.module.ts
+++ projects/web-app/src/app/dialogs/modules/note-modification-dialog/note-modification-dialog.module.ts
@@ -7,6 +7,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EditorModule } from 'common-library/lib/editor/editor.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { AutocompleteInputFormModule } from '@composite-library/lib/forms/autocomplete-input/autocomplete-input-form.module';
+import { NoteTagModule } from '@composite-library/lib/components/note-tag/note-tag.module';
 
 @NgModule({
   imports: [
@@ -17,6 +19,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     EditorModule,
     MatButtonModule,
     MatDialogModule,
+    AutocompleteInputFormModule,
+    NoteTagModule,
   ],
   declarations: [NoteModificationDialogComponent],
 })
