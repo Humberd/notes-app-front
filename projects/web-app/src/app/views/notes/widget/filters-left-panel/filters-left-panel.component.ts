8f6609bf7f10e0d6c236dd2d72b345efa929e29e
Humberd <humberd2@gmail.com>
2020-04-26 19:35:12 +0200

    Workspaces are not listable

--- projects/web-app/src/app/views/notes/widget/filters-left-panel/filters-left-panel.component.ts
+++ projects/web-app/src/app/views/notes/widget/filters-left-panel/filters-left-panel.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TagsRefresherService } from '@web-app/app/views/notes/service/tags-refresher.service';
 import { TagView } from '@domain/entity/tag/view/tag-view';
 import { NotesRefresherService } from '@web-app/app/views/notes/service/notes-refresher.service';
+import { WorkspacesRefresherService } from '@web-app/app/views/notes/service/workspaces-refresher.service';
+import { WorkspaceView } from '@domain/entity/workspace/view/workspace-view';
 
 @Component({
   selector: 'app-filters-left-panel',
@@ -14,19 +16,24 @@ export class FiltersLeftPanelComponent implements OnInit {
   constructor(
     public tagsRefresherService: TagsRefresherService,
     public notesRefresherService: NotesRefresherService,
+    public workspacesRefresherService: WorkspacesRefresherService,
   ) {
   }
 
   ngOnInit(): void {
   }
 
-  trackBy(index: number, tagView: TagView) {
+  trackByTag(index: number, tagView: TagView) {
     return tagView.id;
+  }
+
+  trackByWorkspace(index: number, workspaceView: WorkspaceView) {
+    return workspaceView.id;
   }
 
   chooseTag(tag: TagView) {
     if (this.notesRefresherService.tagIds.some(it => it === tag.id)) {
-      this.notesRefresherService.filterByTags(this.notesRefresherService.tagIds.filter(it => it !== tag.id))
+      this.notesRefresherService.filterByTags(this.notesRefresherService.tagIds.filter(it => it !== tag.id));
     } else {
       this.notesRefresherService.filterByTags([...this.notesRefresherService.tagIds, tag.id]);
     }
